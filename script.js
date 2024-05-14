ec91e72d140e7d40fbf3ff4b9ec5914b7b55aedf
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2024-05-14 06:14:10 -0300

    Atualização de tamnhos e cores da font das mensagens

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function menssol(){
 function mensflor1(){
     document.getElementById('dia-maes').style.visibility = 'hidden';
     document.getElementById('p-maes').style.visibility = 'hidden';
-    document.getElementById('mens').style.backgroundColor = '#663399';
+    document.getElementById('mens').style.backgroundColor = '#7f40be';
     document.getElementById('mens').classList.add('mostrar');
     document.getElementById('p-mens').style.display = 'block';
     document.getElementById('p-mens').innerHTML = 'Por que eu te amo? <br><br> Porque você me ensinou a amar me amando, eu te amo porque você me amou primeiro. <br> Desde quando eu estava em seu ventre você me deu carinho, conversava comigo e cuidava de mim, me dava amor mesmo sem nunca ter me visto, mesmo sem saber como eu seria. <br> E mesmo tendo certeza do tanto de trabalho que eu iria te dar, de todas as fraudas que teria que limpar e de todas as vezes que teria que enxugar o meu choro. <br> Mesmo assim você me amou. <br><br>';
@@ -25,7 +25,7 @@ function mensflor1(){
 function mensflor2(){
     document.getElementById('dia-maes').style.visibility = 'hidden';
     document.getElementById('p-maes').style.visibility = 'hidden';
-    document.getElementById('mens').style.backgroundColor = '#D2527F';
+    document.getElementById('mens').style.backgroundColor = '#f35f93';
     document.getElementById('mens').classList.add('mostrar');
     document.getElementById('p-mens').style.display = 'block';
     document.getElementById('p-mens').innerHTML = 'Por que eu te amo? <br><br> Te amo porque você sempre me protejeu, do frio e do calor, quando eu era bebe e não conseguia nem mesmo me trocar. <br> Me protegeu dos perigos do mundo, quando me dava a mão para atravessar a rua, ou quando me impedia de maxucar a mim mesmo. <br> Te amo porque você sempre me ajudou em tudo, nas lições da escola e em tantas outras situações. <br> Me ensinou a ter educação, responsabilidade e determinação. <br> Quando me dizem que sou uma boa pessoa, isso não é graças a mim mesmo, mas sim a você e ao papai que sempre me ensinaram o que é certo.<br><br>';
@@ -37,7 +37,7 @@ function mensflor2(){
 function mensflor3(){
     document.getElementById('dia-maes').style.visibility = 'hidden';
     document.getElementById('p-maes').style.visibility = 'hidden';
-    document.getElementById('mens').style.backgroundColor = '#b21c0e';
+    document.getElementById('mens').style.backgroundColor = '#b82a1d';
     document.getElementById('mens').classList.add('mostrar');
     document.getElementById('p-mens').style.display = 'block';
     document.getElementById('p-mens').innerHTML = 'Por que eu te amo? <br><br> Porque você me suportou e me ouviu. <br> Você suportou os meu choros de bebe, as minhas artes de criança, as minhas chatices de adolescente <br> E suporta até hoje o mala sem alça que sou, que não gosta muito de abraços e beijos ou fotos, o rapaz que tem muita saudade do passado, que vive reclamando do presente e que morre de medo do futuro. <br> Você sempre me ouve e me entende, as vezes sem mesmo eu dizer uma palavra. <br> Só espero um dia compensar tanto sacrifício, e orgular você e o papai. <br> Te amo muito mamãe, as vezes eu esqueço de dizer, mas saiba que eu te amo muito.<br><br>';
